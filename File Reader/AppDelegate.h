6867a44f9c195b61a59db946cf14361d7ad54c36
Rajesh <rajeshkumarit90@gmail.com>
2014-03-28 12:15:37 +0530

    Initial Commit

---
+++ File Reader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  File Reader
+//
+//  Created by Rajesh on 28/03/14.
+//  Copyright (c) 2014 TechDevMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ File Reader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  File Reader
+//
+//  Created by Rajesh on 28/03/14.
+//  Copyright (c) 2014 TechDevMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
